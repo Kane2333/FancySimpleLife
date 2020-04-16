2b3cfe4d31eea6794f6f1f29920698ba3f624f91
YIPIN JIN <yjin0026@student.monash.edu>
2020-04-16 23:19:22 +1000

    e0.0.4
    Auto orientation now disabled.
    Account detail page implemented.

---
+++ MySimpleTest/MySimpleTest/Custom Views/Views/FLAccountButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  FLAccountButtonView.swift
+//  MySimpleTest
+//
+//  Created by YIPIN JIN on 16/4/20.
+//  Copyright © 2020 FancyLife. All rights reserved.
+//
+
+import UIKit
+
+class FLAccountButtonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
